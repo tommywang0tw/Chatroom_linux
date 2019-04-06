cf88e6ce4c7796353ed9bcfa82b9edbd7a7c24e5
tommywang0tw <tommywang0.tw@gmail.com>
2019-04-06 14:44:41 +0800

    Keep communicating with server
    In order to implement the functionality of a live chatroom, we need to
    keep commnicating with the server. Moreover, we need to be able to send
    message to the server and receive the message from other clients. The
    server will broadcast the message it gets from other clients, all we
    need to do is to read the message sent by the server.
    
    - Use "select" to monitor we need to do "read" or "write"
    
    - Use a infinite loop to keep communicating with the server until
    disconnection

--- OS_client.c
+++ OS_client.c
@@ -5,45 +5,78 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#define SERVER_IP "127.0.0.1"
 int main()
 {
     struct sockaddr_in server;
     int sock;
     char *msg = "Message back!\n";
     char buf[32];
-    int n;
-    
-    /* 製作 socket */
+    int n, i;
+    fd_set file_descriptors;
+    /* Create a socket */
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    
-    /* 準備連線端指定用的 struct 資料 */
+
     server.sin_family = AF_INET;
     server.sin_port = htons(12345);
+    /* 127.0.0.1 is the IP address of local host */
+    inet_pton(AF_INET, SERVER_IP, &server.sin_addr.s_addr);
     
-    /* 127.0.0.1 是 localhost 本機位址 */
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr.s_addr);
-    
-    /* 與 server 端連線 */
-    connect(sock, (struct sockaddr *)&server, sizeof(server));
-    
-    /* 從伺服器接受資料 */
-    memset(buf, 0, sizeof(buf));
-    n = read(sock, buf, sizeof(buf));
-    
-    printf("\t[Info] Receive %d bytes: %s\n", n, buf);
-    
-    /*Send message to server*/
-    n = write(sock, msg, strlen(msg));
-    printf("\t[info] Send %d bytes: %s\n", n, msg);
-    
-    /*Send a message typed by user to server*/
-    memset(buf, 0, sizeof(buf));
-    fgets(buf, sizeof(buf), stdin);
-    n = write(sock, buf, sizeof(buf));
-    printf("\t[Info] Send %d bytes: %s\n", n, buf);
-
-    /* 結束 socket */
+    /* connect to the server */
+    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) == -1)
+    {
+        perror("connect failed: ");
+        exit(1);
+    }
+    for(;;)
+    {
+        /* Initialize file descriptor set*/
+        FD_ZERO(&file_descriptors);
+        FD_SET(STDIN_FILENO, &file_descriptors);
+        FD_SET(sock, &file_descriptors);
+        fflush(stdin);
+        if(select(sock+1, &file_descriptors, NULL, NULL, NULL) < 0)
+        {
+            perror("select:");
+            exit(1);
+        }
+        if(FD_ISSET(STDIN_FILENO, &file_descriptors))
+        {
+            //handle user input
+            memset(buf, 0, sizeof(buf));
+            fgets(buf, sizeof(buf), stdin);
+            if((n = write(sock, buf, sizeof(buf))) == -1)
+            {
+                perror("write failed: ");
+            }
+            else
+            {
+                printf("\t[Info] Send %d bytes: %s\n", n, buf);
+            }
+        }
+        if(FD_ISSET(sock, &file_descriptors))
+        {
+            //handle server input
+            memset(buf, 0, sizeof(buf));
+            if((n = read(sock, buf, sizeof(buf))) <= 0)
+            {
+                if(n == 0)
+                {
+                    printf("The server is disconnected\n");
+                    return 0;
+                }
+                else
+                {
+                    perror("read failed: ");
+                }
+            }
+            else
+            {
+                printf("\t[Info] Receive %d bytes: %s\n", n, buf);
+            }
+        }
+    }
     close(sock);
     return 0;
 }
